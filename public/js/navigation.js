096d24a92cb10812425fafdaa2313b044a5e1697
PabloWinter <pablowinter7@hotmail.com>
2019-05-29 17:59:54 -0600

    Added fixed navigation bar to display visited links, and smoother transition between sections

--- public/js/navigation.js
+++ public/js/navigation.js
@@ -12,13 +12,31 @@ sr.reveal('.skills-box', {
     duration: 2000
 })
 
-// Navigation functionality
+//Navigation functionality
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
 
         document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
+            behavior: 'smooth',
+            block: 'center'
         });
     });
 });
+
+//Navigation links active
+const navigation = document.getElementById("navigation");
+const links = navigation.getElementsByClassName("nav-item");
+
+for (let i = 0; i < links.length; i++) {
+    links[i].addEventListener("click", function () {
+        var currentLink = document.getElementsByClassName("active");
+        if (currentLink.length > 0) {
+            currentLink[0].className = currentLink[0].className.replace(" active", "");
+        }
+        this.className += " active";
+    });
+}
+
+
+
